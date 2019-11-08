387dd89bfbcaceed6e67fe2906b5e17587d16dca
the casle <redeyedtreefrog28@gmail.com>
2019-11-07 22:55:14 -0600

    Added Defaults

--- source/CLPProvider.m
+++ source/CLPProvider.m
@@ -6,6 +6,19 @@
     static HBPreferences *sharedProvider;
     dispatch_once(&once, ^{
         sharedProvider = [[HBPreferences alloc] initWithIdentifier:@"com.thecasle.clearlockprefs"];
+        [sharedProvider registerDefaults:@{
+                                        @"kTransparency": @YES,
+                                        @"kClearLockscreen": @YES,
+                                        @"kAlwaysLockBlur": @YES,
+                                        @"kAlwaysNCBlur": @YES,
+                                        @"kFlyInDisabled": @YES,
+                                        @"kHistoryBlur": @10,
+                                        @"kLockBlur": @10,
+                                        @"kHistorySaturation": @12,
+                                        @"kLockSaturation": @12,
+                                        @"kHistoryColorAlpha": @0,
+                                        @"kLockColorAlpha": @0
+                                        }];
     });
     return sharedProvider;
 }
